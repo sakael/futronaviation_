0faee418d10bbd03ce2ecdfec50a05c4c7e6f75a
Sam <sam@Saemers-iMac.local>
2022-03-16 16:16:15 +0100

    Added some changes

--- resources/views/components/partials/newsletter.blade.php
+++ resources/views/components/partials/newsletter.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <h2 class="fs-36 lh-40 extra-bold"><a href="{{route('contact')}}">CONTACT US</a></h2>
+                <h2 class="fs-36 lh-40 extra-bold"><a href="{{route('contact')}}" class="contact-us">CONTACT US</a></h2>
                 <h4 class="fs-28 mt-3">Contact us with any <br>
                     questions or comments.</h4>
             </div>
